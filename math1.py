90ebe0b29087fc72af4548dd4fab87ed105dad3e
hsmin1206 <quftlswp1@gmail.com>
2025-05-27 21:58:45 +0900

    첫 수학예제 연습

---
+++ math1.py
@@ -0,0 +1,5 @@
+my_value = 2 * (3 + 2)**2 / 5 - 4
+print(my_value)
+
+my_value2 = 2 * ((3 + 2)**2 / 5) - 4
+print(my_value2)
---
+++ math2.py
@@ -0,0 +1,10 @@
+x = int(input("숫자를 입력하세요\n"))
+product = 3 * x
+print(product)
+
+beta = 1.75
+theta = 30.0
+
+x1 = 3
+x2 = 10
+x3 = 44

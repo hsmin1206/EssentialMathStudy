df8f2f91c4791900ea8b35dbcdcf3e390324c435
hsmin1206 <quftlswp1@gmail.com>
2025-05-27 22:22:20 +0900

    5/27(화) 개발자 필수수학 공부

---
+++ math3.py
@@ -0,0 +1,9 @@
+# 함수
+def f(x):
+    return 2 * x + 1
+
+x_values = [0, 1, 2, 3]
+
+for x in x_values:
+    y = f(x)
+    print(y)
---
+++ math4.py
@@ -0,0 +1,6 @@
+# 심파이로 선형함수 그래프 그리기
+from sympy import *
+
+x = symbols('x')
+f = 2 * x + 1
+plot(f)

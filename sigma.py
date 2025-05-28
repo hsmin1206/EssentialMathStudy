684bed80807c81c0f10f70a2fdc8595bcb35c2d4
hsmin1206 <quftlswp1@gmail.com>
2025-05-28 22:38:40 +0900

    5/28(수) 개발자 필수수학 공부 추가

--- sigma.py
+++ sigma.py
@@ -7,3 +7,15 @@ n =len(x)
 
 summation1 = sum(10*x[i] for i in range(0, n))
 print(summation1)
+
+# 심파이를 이용한 합계 구하기
+from sympy import *
+
+i, n = symbols('i n')
+
+# 1부터 n까지 i를 반복하면서 2를 곱하고 더합니다.
+summation2 = Sum(2*i, (i, 1, n))
+
+# n을 5로 지정하면 숫자 1에서 5까지 반복합니다.
+up_to_5 = summation2.subs(n, 5)
+print(up_to_5.doit())
